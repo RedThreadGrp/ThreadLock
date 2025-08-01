accd810e5ae17fa7a36be5e46828cb662380942d
RedThreadGrp <moore.h.work@gmail.com>
2025-08-01 14:29:25 -0700

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,1 +1,13 @@
+export default function Footer() {
+  return (
+    <footer className="bg-darkblue text-white text-center py-8 text-sm">
+      © 2025 ThreadLock. All Rights Reserved.
+      <div className="mt-3 space-x-4">
+        <a href="#features" className="hover:text-orange">Features</a>
+        <a href="#pricing" className="hover:text-orange">Pricing</a>
+        <a href="#" className="hover:text-orange">Privacy</a>
+      </div>
+    </footer>
+  );
+}
 
