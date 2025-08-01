061912680db038c8d9759e8531ec2169a615e446
RedThreadGrp <moore.h.work@gmail.com>
2025-08-01 13:21:14 -0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        darkblue: "#2c3e50",
+        orange: "#f39c12",
+        lightgray: "#f4f6f8",
+      },
+    },
+  },
+  plugins: [],
+};
