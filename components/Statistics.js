819bbc2c1e75a7a9ef86b3e7dea902a1a65fec43
RedThreadGrp <moore.h.work@gmail.com>
2025-08-03 13:24:53 -0700

    Create Statistics.js

---
+++ components/Statistics.js
@@ -0,0 +1,35 @@
+<section class="text-gray-600 body-font">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="flex flex-wrap -m-4 text-center">
+      
+      <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+        <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
+          <h2 class="title-font font-medium text-3xl text-gray-900">3.8M+</h2>
+          <p class="leading-relaxed">Family Law Cases / Year</p>
+        </div>
+      </div>
+
+      <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+        <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
+          <h2 class="title-font font-medium text-3xl text-gray-900">72%</h2>
+          <p class="leading-relaxed">Cases with Self-Representation</p>
+        </div>
+      </div>
+
+      <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+        <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
+          <h2 class="title-font font-medium text-3xl text-gray-900">100%</h2>
+          <p class="leading-relaxed">Immutable Records</p>
+        </div>
+      </div>
+
+      <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+        <div class="border-2 border-gray-200 px-4 py-6 rounded-lg">
+          <h2 class="title-font font-medium text-3xl text-gray-900">5 Min</h2>
+          <p class="leading-relaxed">Avg Time to Court-Ready Export</p>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</section>
