d8a7e541495e0b08589559de93c45c11037995d6
RedThreadGrp <moore.h.work@gmail.com>
2025-08-03 14:24:47 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        darkblue: "#2c3e50",
-        orange: "#f39c12",
-        lightgray: "#f4f6f8",
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
