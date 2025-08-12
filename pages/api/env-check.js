54daca8403543a27d834b44ad1b4996b91d5624a
RedThreadGrp <moore.h.work@gmail.com>
2025-08-12 10:47:57 -0700

    Create env-check.js

---
+++ pages/api/env-check.js
@@ -0,0 +1,7 @@
+export default function handler(req, res) {
+  res.status(200).json({
+    SUPABASE_URL: !!process.env.SUPABASE_URL,
+    SUPABASE_SERVICE_ROLE_KEY: !!process.env.SUPABASE_SERVICE_ROLE_KEY,
+    STRIPE_SECRET_KEY: !!process.env.STRIPE_SECRET_KEY,
+  });
+}
