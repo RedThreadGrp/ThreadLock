bfd0932d97d16e27cc6c76dce6c4d519acdc9cc5
RedThreadGrp <moore.h.work@gmail.com>
2025-08-20 22:17:34 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
-import '../styles/globals.css'
-
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// pages/_app.js
+import "@/styles/globals.css";
+import { Plus_Jakarta_Sans } from "next/font/google";
+const font = Plus_Jakarta_Sans({ subsets: ["latin"], weight: ["500","700","800"] });
+export default function App({ Component, pageProps }) {
+  return <main className={font.className}><Component {...pageProps} /></main>;
 }
